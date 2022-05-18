266563196ef7da906e4422e5d42a5a2186e3d41a
Sapein <5852983+Sapein@users.noreply.github.com>
2022-05-18 15:21:15 -0500

    Formatting

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,5 @@
-use regex::Regex;
 use lazy_static::lazy_static;
+use regex::Regex;
 
 pub mod tokens;
 use tokens::Token;
@@ -11,7 +11,7 @@ lazy_static! {
     static ref WHI: Regex = Regex::new(r"\s").unwrap();
 }
 
-pub fn scan_string(input: String) -> Vec<Token>{
+pub fn scan_string(input: String) -> Vec<Token> {
     let mut curr_str = String::new();
     let mut tokens = vec![];
 
@@ -25,67 +25,70 @@ pub fn scan_string(input: String) -> Vec<Token>{
                     } else {
                         false
                     }
-                },
-                None => false
+                }
+                None => false,
             };
 
-            if WHI.is_match(&c.to_string()) && curr_str.chars().count() > 0{
+            if WHI.is_match(&c.to_string()) && curr_str.chars().count() > 0 {
                 let offset = if ci >= curr_str.chars().count() {
                     curr_str.chars().count()
                 } else {
                     0
                 };
 
-                tokens.push(Token::new(li, ci-offset, curr_str));
+                tokens.push(Token::new((li, li), (ci - offset, ci - 1), curr_str));
                 curr_str = String::new();
                 continue;
             }
 
-            if is_special_char(&curr_str, complex_special) && c != '@'{
-                tokens.push(Token::new(li, ci - curr_str.chars().count(), curr_str));
+            if is_special_char(&curr_str, complex_special) && c != '@' {
+                tokens.push(Token::new((li, li), (ci - curr_str.chars().count(), ci -1), curr_str));
                 curr_str = String::new();
             }
 
-            if is_special_char(&c.to_string(), complex_special){
+            if is_special_char(&c.to_string(), complex_special) {
                 if curr_str.chars().count() > 1 {
-                    tokens.push(Token::new(li, ci - curr_str.chars().count(), curr_str));
+                    tokens.push(Token::new((li, li), (ci - curr_str.chars().count(), ci), curr_str));
                     curr_str = String::new();
                 } else if curr_str.chars().count() == 1 {
                     if ci > 0 {
-                        tokens.push(Token::new(li, ci - 1, curr_str));
+                        tokens.push(Token::new((li, li), (ci - 1, ci - 1), curr_str));
                     } else {
-                        tokens.push(Token::new(li, ci, curr_str));
+                        tokens.push(Token::new((li, li), (ci, ci), curr_str));
                     }
                     curr_str = String::new();
                 }
 
                 curr_str.push(c);
-                tokens.push(Token::new(li, ci, curr_str));
+                tokens.push(Token::new((li, li), (ci, ci), curr_str));
                 curr_str = String::new();
                 continue;
             }
 
             if kinda_special_char(c) {
-                if curr_str.chars().count() > 0 && curr_str.chars().nth(0).unwrap() != ';' && curr_str.chars().nth(0).unwrap() != '"'{
-                    let offset = if ci >= curr_str.chars().count() { 
-                        curr_str.chars().count() 
-                    } else { 
-                        0 
+                if curr_str.chars().count() > 0
+                    && curr_str.chars().nth(0).unwrap() != ';'
+                    && curr_str.chars().nth(0).unwrap() != '"'
+                {
+                    let offset = if ci >= curr_str.chars().count() {
+                        curr_str.chars().count()
+                    } else {
+                        0
                     };
 
-                    tokens.push(Token::new(li, ci - offset, curr_str));
+                    tokens.push(Token::new((li, li), (ci - offset, ci), curr_str));
                     curr_str = String::new();
                 } else if curr_str.chars().count() > 0 && curr_str.chars().nth(0).unwrap() == ';' {
                     curr_str.push(c);
                     continue;
                 } else if curr_str.chars().count() > 0 && curr_str.chars().nth(0).unwrap() == '"' {
-                    let offset = if ci >= curr_str.chars().count() { 
-                        curr_str.chars().count() 
-                    } else { 
-                        0 
+                    let offset = if ci >= curr_str.chars().count() {
+                        curr_str.chars().count()
+                    } else {
+                        0
                     };
                     curr_str.push(c);
-                    tokens.push(Token::new(li, ci - offset, curr_str));
+                    tokens.push(Token::new((li,li), (ci - offset, ci), curr_str));
                     curr_str = String::new();
                 } else {
                     curr_str.push(c);
@@ -96,7 +99,11 @@ pub fn scan_string(input: String) -> Vec<Token>{
             curr_str.push(c);
         }
         if curr_str.chars().count() > 0 {
-            tokens.push(Token::new(li, line.chars().count() - curr_str.chars().count(), curr_str));
+            tokens.push(Token::new(
+                (li, li),
+                (line.chars().count() - curr_str.chars().count(), line.chars().count() - 1),
+                curr_str,
+            ));
             curr_str = String::new();
         }
     }
@@ -104,10 +111,10 @@ pub fn scan_string(input: String) -> Vec<Token>{
 }
 
 fn kinda_special_char(c: char) -> bool {
-    '\"' == c || ';' == c 
+    '\"' == c || ';' == c
 }
 
-fn is_special_char(c: &str, next_is_at: bool) -> bool{
+fn is_special_char(c: &str, next_is_at: bool) -> bool {
     "(" == c || ")" == c || (c == "," && !next_is_at) || (c == ",@")
 }
 
@@ -131,61 +138,54 @@ mod tests {
             ",123".to_string(),
             ",@123".to_string(),
         ];
+
         let output = [
             vec![
-                Token::new(0, 0, ",@".to_string()),
-                Token::new(0, 2, "(".to_string()),
-                Token::new(0, 3, "a".to_string()),
-                Token::new(0, 5, "b".to_string()),
-                Token::new(0, 7, "c".to_string()),
-                Token::new(0, 8, ")".to_string())
+                Token::new((0,0), (0, 1), ",@".to_string()),
+                Token::new((0,0), (2, 2), "(".to_string()),
+                Token::new((0,0), (3, 3), "a".to_string()),
+                Token::new((0,0), (5, 5), "b".to_string()),
+                Token::new((0,0), (7, 7), "c".to_string()),
+                Token::new((0,0), (8, 8), ")".to_string()),
             ],
             vec![
-                Token::new(0, 0, ",".to_string()),
-                Token::new(0, 1, "(".to_string()),
-                Token::new(0, 2, "a".to_string()),
-                Token::new(0, 4, "b".to_string()),
-                Token::new(0, 6, "c".to_string()),
-                Token::new(0, 7, ")".to_string()),
+                Token::new((0, 0), (0, 0), ",".to_string()),
+                Token::new((0, 0), (1, 1), "(".to_string()),
+                Token::new((0, 0), (2, 2), "a".to_string()),
+                Token::new((0, 0), (4, 4), "b".to_string()),
+                Token::new((0, 0), (6, 6), "c".to_string()),
+                Token::new((0, 0), (7, 7), ")".to_string()),
             ],
             vec![
-                Token::new(0, 0, "(".to_string()),
-                Token::new(0, 1, "a@b".to_string()),
-                Token::new(0, 5, "c".to_string()),
-                Token::new(0, 6, ")".to_string()),
+                Token::new((0, 0), (0, 0), "(".to_string()),
+                Token::new((0, 0), (1, 3), "a@b".to_string()),
+                Token::new((0, 0), (5, 5), "c".to_string()),
+                Token::new((0, 0), (6, 6), ")".to_string()),
             ],
             vec![
-                Token::new(0, 0, "(".to_string()),
-                Token::new(0, 1, "a".to_string()),
-                Token::new(0, 2, ",".to_string()),
-                Token::new(0, 3, "b".to_string()),
-                Token::new(0, 5, "c".to_string()),
-                Token::new(0, 6, ")".to_string()),
+                Token::new((0, 0), (0, 0), "(".to_string()),
+                Token::new((0, 0), (1, 1), "a".to_string()),
+                Token::new((0, 0), (2, 2), ",".to_string()),
+                Token::new((0, 0), (3, 3), "b".to_string()),
+                Token::new((0, 0), (5, 5), "c".to_string()),
+                Token::new((0, 0), (6, 6), ")".to_string()),
+            ],
+            vec![Token::new((0, 0), (0, 5), "\"test\"".to_string())],
+            vec![Token::new((0, 0), (0, 3), "1234".to_string())],
+            vec![Token::new((0, 0), (0, 3), "0123".to_string())],
+            vec![Token::new((0, 0), (0, 7), ";comment".to_string())],
+            vec![
+                Token::new((0, 0), (0, 2), "123".to_string()),
+                Token::new((1, 1), (0, 2), "345".to_string()),
             ],
             vec![
-                Token::new(0, 0, "\"test\"".to_string())
+                Token::new((0, 0), (0, 0), ",".to_string()),
+                Token::new((0, 0), (1, 3), "123".to_string()),
             ],
             vec![
-                Token::new(0, 0, "1234".to_string())
+                Token::new((0, 0), (0, 1), ",@".to_string()),
+                Token::new((0, 0), (2, 4), "123".to_string()),
             ],
-            vec![
-                Token::new(0, 0, "0123".to_string())
-            ],
-            vec![
-                Token::new(0, 0, ";comment".to_string())
-            ],
-            vec![
-                Token::new(0, 0, "123".to_string()),
-                Token::new(1, 0, "345".to_string())
-            ],
-            vec![
-                Token::new(0, 0, ",".to_string()),
-                Token::new(0, 1, "123".to_string())
-            ],
-            vec![
-                Token::new(0, 0, ",@".to_string()),
-                Token::new(0, 2, "123".to_string())
-            ]
         ];
 
         for (index, string) in input.iter().enumerate() {
@@ -195,7 +195,7 @@ mod tests {
     }
 
     #[test]
-    fn test_is_special(){
+    fn test_is_special() {
         assert!(is_special_char("(", false));
         assert!(is_special_char("(", true));
 
@@ -222,6 +222,5 @@ mod tests {
 
         assert!(kinda_special_char(';'));
         assert!(kinda_special_char(';'));
-
     }
 }
--- src/lexer/tokens.rs
+++ src/lexer/tokens.rs
@@ -1,29 +1,31 @@
 #[derive(Debug, PartialEq)]
 pub enum TokenType {
     // Composite Tokens
-    Atom(String), Number(u128), Str(String), True(bool), False(bool), UnquoteSplice,
+    Atom(String), Number(u128), Str(String),
+    True(bool), False(bool), UnquoteSplice,
 
     // Single Characters
     LeftParen, RightParen, Comma, At,
-    Backquote, SingleQuote, DoubleQuote,
+    Backquote, SingleQuote,
     Comment(String),
 
     // Whitespace Characters
     Newline, Tab,
+    Whitespace(String),
 
-    EOF
+    EOF,
 }
 
 #[derive(Debug, PartialEq)]
 pub struct Token {
-    row: usize,
-    col: usize,
+    row: (usize, usize),
+    col: (usize, usize),
 
     _type: TokenType,
 }
 
 impl Token {
-    pub fn new(row: usize, col: usize, raw_characters: String) -> Token {
+    pub fn new(row: (usize, usize), col: (usize, usize), raw_characters: String) -> Token {
         Token {
             row: row,
             col: col,
@@ -41,11 +43,37 @@ impl Token {
                 "\n" => TokenType::Newline,
                 "\t" => TokenType::Tab,
                 _ if super::STR.is_match(raw_characters.as_str()) => TokenType::Str(raw_characters),
-                _ if super::NUM.is_match(raw_characters.as_str()) => TokenType::Number(raw_characters.parse::<u128>().unwrap()),
-                _ if super::COM.is_match(raw_characters.as_str()) => TokenType::Comment(raw_characters),
+                _ if super::NUM.is_match(raw_characters.as_str()) => {
+                    TokenType::Number(raw_characters.parse::<u128>().unwrap())
+                }
+                _ if super::COM.is_match(raw_characters.as_str()) => {
+                    TokenType::Comment(raw_characters)
+                }
+                _ if super::WHI.is_match(raw_characters.as_str()) => {
+                    TokenType::Whitespace(raw_characters)
+                }
                 _ => TokenType::Atom(raw_characters),
             },
-
         }
     }
+
+    pub fn break_apart(self) -> ((usize, usize), (usize, usize), TokenType) {
+        (self.row, self.col, self._type)
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_breakapart() {
+        let token = Token::new((0, 0), (0, 0), "(".to_string());
+        let token_str = Token::new((0, 0), (0, 3) , "asdf".to_string());
+        let token_num = Token::new((0, 0), (0, 2), "123".to_string());
+        assert_eq!(token.break_apart(), ((0,0), (0,0), TokenType::LeftParen));
+        assert_eq!(token_str.break_apart(), ((0, 0), (0,3), TokenType::Atom("asdf".to_string())));
+        assert_eq!(token_num.break_apart(), ((0,0), (0,2), TokenType::Number(123)))
+    }
+
 }
